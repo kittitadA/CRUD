52cfc9496a1a47de21d71cbcb3b11b4e52d2c3be
kittitadA <kittitad.taz@gmail.com>
2019-03-16 01:13:16 +0700

    Updates

---
+++ precache-manifest.66169b13a85bda684a92519ea19877d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "23812e095c0ad72d6572",
+    "url": "/CRUD/static/css/main.da669ec1.chunk.css"
+  },
+  {
+    "revision": "23812e095c0ad72d6572",
+    "url": "/CRUD/static/js/main.23812e09.chunk.js"
+  },
+  {
+    "revision": "937054bc6d41ca898a3d",
+    "url": "/CRUD/static/js/runtime~main.937054bc.js"
+  },
+  {
+    "revision": "dcb82aee628a7a110fea",
+    "url": "/CRUD/static/js/2.dcb82aee.chunk.js"
+  },
+  {
+    "revision": "3cd852e925233731f690755e267bf28c",
+    "url": "/CRUD/index.html"
+  }
+];
